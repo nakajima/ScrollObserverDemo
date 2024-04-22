07f670f15c69e2dff7dadbc603205354a4a1c77b
Pat Nakajima <patnakajima@gmail.com>
2024-04-22 15:19:03 -0700

    Initial Commit

---
+++ ScrollObserverDemo/ScrollObserverDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollObserverDemoApp.swift
+//  ScrollObserverDemo
+//
+//  Created by Pat Nakajima on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollObserverDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
